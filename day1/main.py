6477cb2409a079a585323ceb5e1bf804669320b9
Luc Rubio <lrr@google.com>
2019-12-01 23:21:14 +0000

    Day 1 in Python

---
+++ day1/main.py
@@ -0,0 +1,7 @@
+import math
+
+with open('input') as mass_list:
+    total_fuel_req = 0
+    for mass in mass_list:
+        total_fuel_req += math.floor(int(mass) / 3) - 2
+    print(int(total_fuel_req))
