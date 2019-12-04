56d166c509d01fc889904d8d0bb0002de4ac02bd
Luc Rubio <lrr@google.com>
2019-12-04 17:09:37 +0000

    Better names in day 4

--- day04/main.py
+++ day04/main.py
@@ -1,53 +1,47 @@
-def has_repeated_adjacent(number):
-    number_str = str(number)
-    digit = number_str[0]
-    for i in range(1, 6):
-        if number_str[i] == digit:
+def has_repeated_adjacent(num):
+    number_str = str(num)
+    last_digit = number_str[0]
+    for i in range(1, len(number_str)):
+        if number_str[i] == last_digit:
             return True
         else:
-            digit = number_str[i]
+            last_digit = number_str[i]
     return False
 
 
-def has_a_double_adjacent(number):
-    number_str = str(number)
-    digit = number_str[0]
-    count = 1
-    for i in range(1, 6):
-        if number_str[i] == digit:
-            count += 1
+def has_a_double_adjacent(num):
+    number_str = str(num)
+    last_digit = number_str[0]
+    group_len = 1
+    for i in range(1, len(number_str)):
+        if number_str[i] == last_digit:
+            group_len += 1
         else:
-            if count == 2:
+            if group_len == 2:
                 return True
-            digit = number_str[i]
-            count = 1
-    return count == 2
+            last_digit = number_str[i]
+            group_len = 1
+    return group_len == 2
 
 
-def is_non_decreasing(number):
-    number_str = str(number)
-    digit = number_str[0]
-    for i in range(1, 6):
-        if number_str[i] < digit:
+def is_non_decreasing(num):
+    number_str = str(num)
+    last_digit = number_str[0]
+    for i in range(1, len(number_str)):
+        if number_str[i] < last_digit:
             return False
-        digit = number_str[i]
+        last_digit = number_str[i]
     return True
 
 
 def part_one(start, end):
-    count = 0
-    for num in range(start, end + 1):
-        if has_repeated_adjacent(num) and is_non_decreasing(num):
-            count += 1
-    return count
+    return len([num for num in range(start, end + 1) if
+                has_repeated_adjacent(num) and is_non_decreasing(num)])
 
 
 def part_two(start, end):
-    count = 0
-    for num in range(start, end + 1):
-        if has_a_double_adjacent(num) and is_non_decreasing(num):
-            count += 1
-    return count
+    return len([num for num in range(start, end + 1) if
+                has_a_double_adjacent(num) and is_non_decreasing(num)])
 
 
 print(part_one(256310, 732736))
