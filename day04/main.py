20cf01300acc8f53c4a35de0009615475de3b0b3
Luc Rubio <lrr@google.com>
2019-12-05 20:45:30 +0000

    Incorporate advice from /u/Stringhe

--- day04/main.py
+++ day04/main.py
@@ -1,11 +1,11 @@
 def has_repeated_adjacent(num):
     number_str = str(num)
     last_digit = number_str[0]
-    for i in range(1, len(number_str)):
-        if number_str[i] == last_digit:
+    for digit in number_str[1:]:
+        if digit == last_digit:
             return True
         else:
-            last_digit = number_str[i]
+            last_digit = digit
     return False
 
 
@@ -13,13 +13,13 @@ def has_a_double_adjacent(num):
     number_str = str(num)
     last_digit = number_str[0]
     group_len = 1
-    for i in range(1, len(number_str)):
-        if number_str[i] == last_digit:
+    for digit in number_str[1:]:
+        if digit == last_digit:
             group_len += 1
         else:
             if group_len == 2:
                 return True
-            last_digit = number_str[i]
+            last_digit = digit
             group_len = 1
     return group_len == 2
 
@@ -27,10 +27,10 @@ def has_a_double_adjacent(num):
 def is_non_decreasing(num):
     number_str = str(num)
     last_digit = number_str[0]
-    for i in range(1, len(number_str)):
-        if number_str[i] < last_digit:
+    for digit in number_str[1:]:
+        if digit < last_digit:
             return False
-        last_digit = number_str[i]
+        last_digit = digit
     return True
 
 
@@ -44,5 +44,5 @@ def part_two(start, end):
                 has_a_double_adjacent(num) and is_non_decreasing(num)])
 
 
-print(part_one(256310, 732736))
-print(part_two(256310, 732736))
+print(part_one(256310, 732736))  # 979
+print(part_two(256310, 732736))  # 635
