5f3e07df9af782fa65f03b15f194a2e8132d9cae
Luc Rubio <lrr@google.com>
2019-12-10 20:23:15 +0000

    Merge similar functions

--- day10/main.py
+++ day10/main.py
@@ -2,7 +2,7 @@ import math
 from functools import cmp_to_key
 
 
-def get_map(file):
+def _get_map(file):
     map = []
     with open(file) as f:
         for line in f.readlines():
@@ -13,7 +13,7 @@ def get_map(file):
     return map
 
 
-def find_asteroid_pos(map, width, height, x, y, dx, dy):
+def _find_asteroid_pos(map, width, height, x, y, dx, dy):
     pos_x = x + dx
     pos_y = y + dy
     while width > pos_x >= 0 and height > pos_y >= 0:
@@ -24,75 +24,51 @@ def find_asteroid_pos(map, width, height, x, y, dx, dy):
     return None
 
 
-def normalize(x, y):
+def _normalize(x, y):
     gcd = math.gcd(x, y)
     return tuple([x // gcd, y // gcd])
 
 
-def cmp_vector_angle(a, b):
-    ah = math.sqrt(a[0] * a[0] + a[1] * a[1])
-    bh = math.sqrt(b[0] * b[0] + b[1] * b[1])
-    return 1 if math.asin(a[1] / ah) > math.asin(b[1] / bh) else -1
+def _cmp_vector_angle(vector_a, vector_b):
+    ah = math.sqrt(vector_a[0] * vector_a[0] + vector_a[1] * vector_a[1])
+    bh = math.sqrt(vector_b[0] * vector_b[0] + vector_b[1] * vector_b[1])
+    return 1 if math.asin(vector_a[1] / ah) > math.asin(vector_b[1] / bh) else -1
 
 
-def get_first_quadrant(width, height):
+def _get_quadrant_dirs(range_x, range_y, is_angle_negative):
     dirs = set()
-    for w in range(1, width):
-        for h in range(-height + 1, 0):
-            dirs.add(normalize(w, h))
-    return sorted(dirs, key=cmp_to_key(cmp_vector_angle))
+    for w in range_x:
+        for h in range_y:
+            if w == 0 and h == 0:
+                continue
+            dirs.add(_normalize(w, h))
+    return sorted(dirs, key=cmp_to_key(_cmp_vector_angle), reverse=is_angle_negative)
 
 
-def get_second_quadrant(width, height):
-    dirs = set()
-    for w in range(-width + 1, 0):
-        for h in range(-height + 1, 0):
-            dirs.add(normalize(w, h))
-    return sorted(dirs, key=cmp_to_key(cmp_vector_angle), reverse=True)
-
-
-def get_third_quadrant(width, height):
-    dirs = set()
-    for w in range(-width + 1, 0):
-        for h in range(1, height):
-            dirs.add(normalize(w, h))
-    return sorted(dirs, key=cmp_to_key(cmp_vector_angle), reverse=True)
-
-
-def get_fourth_quadrant(width, height):
-    dirs = set()
-    for w in range(1, width):
-        for h in range(1, height):
-            dirs.add(normalize(w, h))
-    return sorted(dirs, key=cmp_to_key(cmp_vector_angle))
-
-
-def calc_directions(width, height):
+def _calc_directions(width, height):
     sorted_dirs = []
-    sorted_dirs.append(normalize(0, -height))  # Top
-    sorted_dirs.extend(get_first_quadrant(width, height))
-    sorted_dirs.append(normalize(width - 1, 0))  # Right
-    sorted_dirs.extend(get_fourth_quadrant(width, height))
-    sorted_dirs.append(normalize(0, height))  # Bottom
-    sorted_dirs.extend(get_third_quadrant(width, height))
-    sorted_dirs.append(normalize(-width, 0))  # Left
-    sorted_dirs.extend(get_second_quadrant(width, height))
+    # Clockwise order: quadrant 1, 4, 3 and 2
+    range_x = [range(0, width), range(0, width), range(-width, 0), range(-width, 0)]
+    range_y = [range(-height, 0), range(0, height), range(0, height), range(-height, 0)]
+    negative_angle = [False, False, True, True]
+    for q in range(0, 4):
+        sorted_dirs.extend(_get_quadrant_dirs(range_x[q], range_y[q], negative_angle[q]))
     return sorted_dirs
 
 
-def get_monitoring_station_position(filename):
+def _get_monitoring_station_position(filename):
     best_position = None
     max_asteroids = 0
-    map = get_map('input')
+    map = _get_map(filename)
     width = len(map[0])
     height = len(map)
-    directions = calc_directions(width, height)
+    directions = _calc_directions(width, height)
     for y in range(0, height):
         for x in range(0, width):
             if map[y][x] == '#':
                 count_asteroids = 0
                 for vector in directions:
-                    pos = find_asteroid_pos(map, width, height, x, y, vector[0], vector[1])
+                    pos = _find_asteroid_pos(map, width, height, x, y, vector[0], vector[1])
                     if pos is not None:
                         count_asteroids += 1
                 if count_asteroids > max_asteroids:
@@ -102,18 +78,18 @@ def get_monitoring_station_position(filename):
 
 
 def part_one(filename):
-    return get_monitoring_station_position(filename)[0]
+    return _get_monitoring_station_position(filename)[0]
 
 
 def part_two(filename, vaporized_num):
-    map = get_map('input')
+    map = _get_map('input')
     width, height = len(map[0]), len(map)
-    directions = calc_directions(width, height)
-    max_asteroids, station_pos = get_monitoring_station_position(filename)
+    directions = _calc_directions(width, height)
+    max_asteroids, station_pos = _get_monitoring_station_position(filename)
     vaporized_count = 0
     while vaporized_count < vaporized_num:
         for vector in directions:
-            asteroid_pos = find_asteroid_pos(map, width, height, station_pos[0], station_pos[1], vector[0], vector[1])
+            asteroid_pos = _find_asteroid_pos(map, width, height, station_pos[0], station_pos[1], vector[0], vector[1])
             if asteroid_pos is not None:
                 map[asteroid_pos[1]][asteroid_pos[0]] = '.'  # vaporized
                 vaporized_count += 1
