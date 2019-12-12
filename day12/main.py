61b179145c3ea4b251fd2776928e311f8f8e5627
Luc Rubio <lrr@google.com>
2019-12-12 16:55:09 +0000

    Day 12 part two in Python 3

--- day12/main.py
+++ day12/main.py
@@ -1,7 +1,21 @@
+import math
+
+
 class Universe:
 
     def __init__(self):
         self.moons = []
+        self.states = set()
+
+    @staticmethod
+    def _lcm(a, b):
+        return abs(a*b) // math.gcd(a, b)
+
+    def _hash_state(self, axis: int):
+        moon_states = []
+        for moon in self.moons:
+            moon_states.append('{} {}'.format(moon.pos[axis], moon.vel[axis]))
+        return '\n'.join(moon_states)
 
     def add_moon(self, moon):
         self.moons.append(moon)
@@ -19,15 +33,49 @@ class Universe:
                         self.moons[i].update_vel(axis, 1)
                         self.moons[j].update_vel(axis, -1)
 
+    def apply_gravity_axis(self, axis: int):
+        for i in range(0, len(self.moons) - 1):
+            for j in range(i + 1, len(self.moons)):
+                pos_i = self.moons[i].pos[axis]
+                pos_j = self.moons[j].pos[axis]
+                if pos_i > pos_j:
+                    self.moons[i].update_vel(axis, -1)
+                    self.moons[j].update_vel(axis, 1)
+                elif pos_i < pos_j:
+                    self.moons[i].update_vel(axis, 1)
+                    self.moons[j].update_vel(axis, -1)
+
     def apply_velocity(self):
         for moon in self.moons:
             for axis in range(0, 3):
                 moon.pos[axis] += moon.vel[axis]
 
+    def apply_velocity_axis(self, axis: int):
+        for moon in self.moons:
+            moon.pos[axis] += moon.vel[axis]
+
     def simulate(self, steps: int):
         for step in range(0, steps):
             self.apply_gravity()
             self.apply_velocity()
+
+    def simulate_axis(self, axis: int):
+        states = set()
+        hash_state = self._hash_state(axis)
+        count = 0
+        while hash_state not in states:
+            states.add(hash_state)
+            self.apply_gravity_axis(axis)
+            self.apply_velocity_axis(axis)
+            count += 1
+            hash_state = self._hash_state(axis)
+        return count
+
+    def calc_steps(self):
+        length_x = self.simulate_axis(0)
+        length_y = self.simulate_axis(1)
+        length_z = self.simulate_axis(2)
+        return self._lcm(self._lcm(length_x, length_y), length_z)
 
     def calc_total_energy(self) -> int:
         total_energy = 0
@@ -67,4 +115,10 @@ def part_one(filename: str, steps: int):
     return universe.calc_total_energy()
 
 
+def part_two(filename: str):
+    universe = _load_moons_from_file(filename)
+    return universe.calc_steps()
+
+
 print(part_one('input', 1000))  # 6735
+print(part_two('input'))  # 326489627728984
