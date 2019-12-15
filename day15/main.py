d063b30b05446800d2e7066c980217bd3e641b78
Luc Rubio <lrr@google.com>
2019-12-15 23:13:55 +0000

    Day 15 part two in Python 3

--- day15/main.py
+++ day15/main.py
@@ -39,12 +39,11 @@ def do_move(vm: Intcode, move: int) -> int:
     return vm.run()
 
 
-def find_distance_to_oxygen(vm: Intcode) -> int:
-    walls = set()
-    visited = set()
+def find_oxygen(vm: Intcode, visited: set, walls: set, complete_map: bool) -> tuple:
     curr_pos = (0, 0)
     visited.add(curr_pos)
     move_stack = []
+    oxygen_pos = None
     while True:
         open_paths = get_possible_moves(visited, walls, curr_pos)
         if len(open_paths) > 0:
@@ -53,23 +52,57 @@ def find_distance_to_oxygen(vm: Intcode) -> int:
             new_pos = get_new_pos(curr_pos, move)
             if result == 0:
                 walls.add(new_pos)
-            elif result == 1:
+            elif result == 1 or result == 2:
                 curr_pos = new_pos
                 move_stack.append(move)
                 visited.add(new_pos)
-            elif result == 2:
-                move_stack.append(move)
-                break  # found oxygen
+                if result == 2:
+                    oxygen_pos = curr_pos
+                    if not complete_map:
+                        break  # found oxygen
         else:
+            if complete_map and curr_pos == (0, 0):
+                break
             opposite_move = get_opposite_move(move_stack.pop())
             do_move(vm, opposite_move)  # ignore output
             curr_pos = get_new_pos(curr_pos, opposite_move)
-    return len(move_stack)
+    return len(move_stack), oxygen_pos
 
 
-def part_one(filename: str):
+def bsf_levels(paths: set, walls: set, start_pos: tuple) -> int:
+    visited = set()
+    queue = [start_pos]
+    levels = 0
+    while len(queue) > 0:
+        size = len(queue)
+        while size > 0:
+            curr_pos = queue.pop(0)
+            visited.add(curr_pos)
+            for d in range(1, 5):
+                new_pos = get_new_pos(curr_pos, d)
+                if new_pos in paths and new_pos not in walls:
+                    if new_pos not in visited:
+                        queue.append(new_pos)
+            size -= 1
+        levels += 1
+    return levels - 1  # do not count root oxygen
+
+
+def part_one(filename: str) -> int:
     vm = Intcode(read_program(filename))
-    return find_distance_to_oxygen(vm)
+    visited = set()
+    walls = set()
+    distance, pos = find_oxygen(vm, visited, walls, False)
+    return distance
+
+
+def part_two(filename: str) -> int:
+    vm = Intcode(read_program(filename))
+    visited = set()
+    walls = set()
+    distance, start_pos = find_oxygen(vm, visited, walls, True)
+    return bsf_levels(visited, walls, start_pos)
 
 
 print(part_one('input'))
+print(part_two('input'))
