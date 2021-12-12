aee9fd3f813c9a78aa59351ae804aa1b2e5c2c4d
Luc Rubio <lrr@google.com>
2021-12-12 22:52:28 +0100

    AOC 2021 Day 06 Part 1

---
+++ aoc2021/src/day06/solution.py
@@ -0,0 +1,62 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+class LanternFish:
+    """Represents a lanternfish."""
+    _NEWBORN_TIMER = 8
+    _SPAWN_TIMER = 6
+
+    timer: int
+
+    def __init__(self, timer: int = _NEWBORN_TIMER) -> None:
+        self.timer = timer
+
+    def run(self) -> bool:
+        """Runs timer on lanternfish.
+
+        Returns:
+            True if the lanternfish spawns a new one, False otherwise.
+        """
+        if self.timer == 0:
+            self.timer = self._SPAWN_TIMER
+            return True
+        self.timer -= 1
+        return False
+
+
+def part_one(lanternfish_ages: str, days: int) -> int:
+    """AOC 2021 Day 6 Part 1.
+
+    Args:
+        lanternfish_ages: Comma-separated string of lanternfish ages.
+        days: Number of elapsed days to calculate population of lanternfish.
+    Returns:
+        Number of points where at least 2 segments overlap.
+    """
+    initial_state = tuple(map(int, lanternfish_ages[0].split(',')))
+    population = [LanternFish(timer=timer) for timer in initial_state]
+    today = 0
+    while today < days:
+        spawn_count = 0
+        for fish in population:
+            spawn_count += 1 if fish.run() else 0
+        population += [LanternFish() for _ in range(0, spawn_count)]
+        today += 1
+    return len(population)
+
+
+def part_two() -> int:
+    """AOC 2021 Day 6 Part 2."""
+    return 0
---
+++ aoc2021/test/day06/test_solution.py
@@ -0,0 +1,36 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import unittest
+
+from aoc2021.src.day06.solution import part_one
+from common.python3.AdventOfCodeTestCase import AdventOfCodeTestCase
+
+
+class TestSolution(AdventOfCodeTestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestSolution, self).__init__(__file__, *args, **kwargs)
+
+    def test_part_one_with_example(self):
+        self.assertEqual(26,
+                         part_one(lanternfish_ages=self.examples[0], days=18))
+        self.assertEqual(5934,
+                         part_one(lanternfish_ages=self.examples[0], days=80))
+
+    def test_part_one_with_input(self):
+        self.assertEqual(349549,
+                         part_one(lanternfish_ages=self.input, days=80))
+
+
+if __name__ == '__main__':
+    unittest.main()
