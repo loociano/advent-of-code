3a8e8ffdd88cca6b55a258f665538da08995aa6f
Luc Rubio <luc@loociano.com>
2022-12-17 23:06:28 +0100

    Optimize

--- aoc2022/src/day15/python/solution.py
+++ aoc2022/src/day15/python/solution.py
@@ -15,6 +15,7 @@ import re
 from typing import Optional, Sequence, TypeAlias
 
 Position: TypeAlias = tuple[int, int]
+Interval: TypeAlias = tuple[int, int]
 
 
 def get_manhattan_distance(position_a: Position, position_b: Position) -> int:
@@ -27,21 +28,38 @@ class Sensor:
     self._closest_beacon_pos = (bx, by)
     self._distance = get_manhattan_distance(position_a=self._sensor_pos,
                                             position_b=self._closest_beacon_pos)
-    self.positions_at_row: set[Position, ...] = self._calc_positions_at_row(
+    self.intervals_at_row: tuple[Interval, ...] = self._calc_intervals_at_row(
       row=row)
 
-  def _calc_positions_at_row(self, row: int) -> Optional[set[Position, ...]]:
+  def _calc_intervals_at_row(self, row: int) -> Optional[tuple[Interval, ...]]:
     if (row < self._sensor_pos[1] - self._distance
         or row > self._sensor_pos[1] + self._distance):
       return None
-    positions = set()
     row_to_sensor = abs(row - self._sensor_pos[1])
     offset_x = self._distance - row_to_sensor
-    for x in range(-offset_x, offset_x + 1):
-      position = (self._sensor_pos[0] - x, row)
-      if position != self._closest_beacon_pos:
-        positions.add(position)
-    return positions
+    min_x = self._sensor_pos[0] - offset_x
+    max_x = self._sensor_pos[0] + offset_x
+    if self._closest_beacon_pos[1] == row:
+      if min_x == self._closest_beacon_pos[0] == max_x:
+        # Edge case.
+        return None
+      if min_x == self._closest_beacon_pos[0]:
+        return (
+          (self._closest_beacon_pos[0] + 1, max_x),
+        )
+      if max_x == self._closest_beacon_pos[0]:
+        return (
+          (min_x, self._closest_beacon_pos[0] - 1),
+        )
+      if min_x < self._closest_beacon_pos[0] < max_x:
+        return (
+          (min_x, self._closest_beacon_pos[0] - 1),
+          (self._closest_beacon_pos[0] + 1, max_x)
+        )
+    else:
+      return (
+        (min_x, max_x),
+      )
 
 
 def _parse_sensors_data(sensors_data: Sequence[str], row: int) -> tuple[Sensor]:
@@ -59,12 +77,33 @@ def _parse_sensors_data(sensors_data: Sequence[str], row: int) -> tuple[Sensor]:
   return tuple(sensors)
 
 
+def _merge_intervals(intervals: tuple[Interval, ...]) -> tuple[Interval, ...]:
+  merged = []
+  for begin, end in sorted(intervals):
+    if len(merged) and merged[-1][1] >= begin - 1:
+      merged[-1][1] = max(merged[-1][1], end)
+    else:
+      merged.append([begin, end])
+  copy = []
+  for i in merged:
+    copy.append(tuple(i))
+  return tuple(merged)
+
+
+def _count_positions(intervals: tuple[Interval, ...]) -> int:
+  count = 0
+  for interval in intervals:
+    count += abs(interval[1] - interval[0] + 1)
+  return count
+
+
 def count_not_beacon_positions(sensors_data: Sequence[str], row: int) -> int:
   sensors: tuple[Sensor] = _parse_sensors_data(sensors_data, row)
   print('Parsed all!')
-  within_beacon_positions = set()
+  intervals = []
   for sensor in sensors:
-    if sensor.positions_at_row is not None:
-      within_beacon_positions = within_beacon_positions.union(
-        sensor.positions_at_row)
-  return len(within_beacon_positions)
+    if sensor.intervals_at_row is not None:
+      for interval_at_row in sensor.intervals_at_row:
+        intervals.append(interval_at_row)
+  merged_intervals = _merge_intervals(tuple(intervals))
+  return _count_positions(merged_intervals)
