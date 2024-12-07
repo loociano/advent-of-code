0fcf7498b548d120be493ed6d6af0b80fa430edb
Luc Rubio <luc@loociano.com>
2024-12-07 15:13:53 +0100

    AoC2024 day 7 part 1

---
+++ aoc2024/src/day07/python/solution.py
@@ -0,0 +1,49 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Sequence
+from itertools import product
+
+
+def _could_be_true(equation_data: str, operators: tuple[str, ...]) -> int | None:
+  """Returns equation result if it can be made true with some combination of addition and multiplication."""
+  result, operands = equation_data.split(': ')
+  result = int(result)
+  operands = operands.split()
+  combinations = list(product(operators, repeat=len(operands) - 1))
+  for combination in combinations:
+    operands_queue = list(map(int, operands))
+    operators_queue = list(combination)
+    computation = operands_queue.pop(0)
+    while len(operands_queue) and computation < result:
+      next_operand = operands_queue.pop(0)
+      next_operator = operators_queue.pop(0)
+      # Cannot use eval() because it follows math precedence rules.
+      if next_operator == '+':
+        computation += next_operand
+      if next_operator == '*':
+        computation *= next_operand
+      if next_operator == '||':
+        computation = int(str(computation) + str(next_operand))
+    if computation == result:
+      return result
+  return None
+
+
+def calc_calibration(equations: Sequence[str], operators: tuple[str, ...] = ('+', '*')) -> int:
+  total_calibration = 0
+  for equation_data in equations:
+    result = _could_be_true(equation_data, operators)
+    if result is not None:
+      total_calibration += result
+  return total_calibration
---
+++ aoc2024/test/day07/python/test_solution.py
@@ -0,0 +1,38 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import unittest
+
+from aoc2024.src.day07.python.solution import calc_calibration
+from common.python3.AdventOfCodeTestCase import AdventOfCodeTestCase
+
+
+class TestDay07Solution(AdventOfCodeTestCase):
+  def __init__(self, *args, **kwargs):
+    (super(TestDay07Solution, self).__init__(__file__, *args, **kwargs))
+
+  def test_part1_withExample_counts(self):
+    self.assertEqual(3749, calc_calibration(self.examples[0]))
+
+  def test_part1_withPuzzleInput_counts(self):
+    self.assertEqual(1611660863222, calc_calibration(self.input))
+
+  def test_part2_withExample_counts(self):
+    self.assertEqual(11387, calc_calibration(self.examples[0], operators=('+', '*', '||')))
+
+  def test_part2_withPuzzleInput_counts(self):
+    self.assertEqual(945341732469724, calc_calibration(self.input, operators=('+', '*', '||')))
+
+
+if __name__ == '__main__':
+  unittest.main()
