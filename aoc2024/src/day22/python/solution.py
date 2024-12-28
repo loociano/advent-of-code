890c738c71edf3df43447d39a50a074f006a5a28
Luc Rubio <luc@loociano.com>
2024-12-28 21:53:59 +0100

    Use functools.cache and refactor

--- aoc2024/src/day22/python/solution.py
+++ aoc2024/src/day22/python/solution.py
@@ -12,22 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from typing import Sequence
+from functools import cache
 
 
 def _mix(secret_number: int, value: int) -> int:
+  """Mixes a value and the secret number."""
   return secret_number ^ value
 
 
 def _prune(secret_number: int) -> int:
+  """Prunes the secret number."""
   return secret_number % 16777216
+
+
+@cache
+def _generate_secret_number(secret_number: int) -> int:
+  result = secret_number
+  result = _prune(_mix(result, result * 64))
+  result = _prune(_mix(result, result // 32))
+  return _prune(_mix(result, result * 2048))
 
 
 def generate_secret_number(secret_number: int, times: int = 1) -> int:
   result = secret_number
   for _ in range(times):
-    result = _prune(_mix(result, result * 64))
-    result = _prune(_mix(result, result // 32))
-    result = _prune(_mix(result, result * 2048))
+    result = _generate_secret_number(result)
   return result
 
 
