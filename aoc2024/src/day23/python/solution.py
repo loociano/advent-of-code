a8717c2d9bb80b9aecf08d9a573b7d8baa83dc7e
Luc Rubio <luc@loociano.com>
2024-12-30 22:18:21 +0100

    AoC 2024 day 23 part 1

---
+++ aoc2024/src/day23/python/solution.py
@@ -0,0 +1,42 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Callable, Sequence
+from collections import defaultdict
+from itertools import combinations
+
+
+def _build_computer_graph(connections: Sequence[str]) -> dict[str, set[str]]:
+  computers: dict[str, set[str]] = defaultdict(set)
+  for connection in connections:
+    first, second = connection.split('-')  # Format is: '{name}-{name}'
+    computers[first].add(second)
+    computers[second].add(first)
+  return computers
+
+
+def _are_interconnected(a: str, b: str, c: str, computers: dict[str, set[str]]) -> bool:
+  return {b, c}.issubset(computers.get(a)) and {a, c}.issubset(computers.get(b)) and {a, b}.issubset(computers.get(c))
+
+
+def _match_any(a, b, c, predicate: Callable) -> bool:
+  return predicate(a) or predicate(b) or predicate(c)
+
+
+def count_computer_sets(connections: Sequence[str], starts_with: str) -> int:
+  computers = _build_computer_graph(connections)
+  return sum(1
+             if (_are_interconnected(a, b, c, computers)
+                 and _match_any(a, b, c, predicate=lambda x: x.startswith(starts_with)))
+             else 0
+             for a, b, c in combinations(computers.keys(), r=3))
---
+++ aoc2024/test/day23/python/test_solution.py
@@ -0,0 +1,33 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import unittest
+
+from common.python3.AdventOfCodeTestCase import AdventOfCodeTestCase
+from aoc2024.src.day23.python.solution import count_computer_sets
+
+
+class TestSolution(AdventOfCodeTestCase):
+  def __init__(self, *args, **kwargs):
+    (super(TestSolution, self).__init__(__file__, *args,
+                                        **kwargs))
+
+  def test_part1_withExample_success(self):
+    self.assertEqual(7, count_computer_sets(connections=self.examples[0], starts_with='t'))
+
+  def test_part1_withPuzzleInput_success(self):
+    self.assertEqual(1344, count_computer_sets(connections=self.input, starts_with='t'))
+
+
+if __name__ == '__main__':
+  unittest.main()
