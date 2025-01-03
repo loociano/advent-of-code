3a1db8d4f1a493fa35aa2e760915e4a5f1b0eddc
Luc Rubio <luc@loociano.com>
2025-01-03 22:58:56 +0100

    AoC 2022 day 6: deprecate read_raw

--- aoc2022/test/day06/python/test_solution.py
+++ aoc2022/test/day06/python/test_solution.py
@@ -19,46 +19,45 @@ from common.python3.AdventOfCodeTestCase import AdventOfCodeTestCase
 
 class TestDay06Solution(AdventOfCodeTestCase):
   def __init__(self, *args, **kwargs):
-    super(TestDay06Solution, self).__init__(
-      __file__, read_raw=True, *args, **kwargs)
+    super().__init__(__file__, *args, **kwargs)
 
   def test_part1_withExampleInput1_returnsMessage(self):
-    self.assertEqual(7, find_marker_position(self.examples[0]))
+    self.assertEqual(7, find_marker_position(self.examples[0][0]))
 
   # TODO: parameterize.
   def test_part1_withExampleInput2_returnsMessage(self):
-    self.assertEqual(5, find_marker_position(self.examples[1]))
+    self.assertEqual(5, find_marker_position(buffer=self.examples[1][0]))
 
   def test_part1_withExampleInput3_returnsMessage(self):
-    self.assertEqual(6, find_marker_position(self.examples[2]))
+    self.assertEqual(6, find_marker_position(buffer=self.examples[2][0]))
 
   def test_part1_withExampleInput4_returnsMessage(self):
-    self.assertEqual(10, find_marker_position(self.examples[3]))
+    self.assertEqual(10, find_marker_position(buffer=self.examples[3][0]))
 
   def test_part1_withExampleInput5_returnsMessage(self):
-    self.assertEqual(11, find_marker_position(self.examples[4]))
+    self.assertEqual(11, find_marker_position(buffer=self.examples[4][0]))
 
   def test_part1_withPuzzleInput_returnsMessage(self):
-    self.assertEqual(1833, find_marker_position(self.input))
+    self.assertEqual(1833, find_marker_position(buffer=self.input[0]))
 
   def test_part2_withExampleInput1_returnsMessage(self):
-    self.assertEqual(19, find_marker_position(self.examples[0], marker_size=14))
+    self.assertEqual(19, find_marker_position(buffer=self.examples[0][0], marker_size=14))
 
   # TODO: parameterize.
   def test_part2_withExampleInput2_returnsMessage(self):
-    self.assertEqual(23, find_marker_position(self.examples[1], marker_size=14))
+    self.assertEqual(23, find_marker_position(buffer=self.examples[1][0], marker_size=14))
 
   def test_part2_withExampleInput3_returnsMessage(self):
-    self.assertEqual(23, find_marker_position(self.examples[2], marker_size=14))
+    self.assertEqual(23, find_marker_position(buffer=self.examples[2][0], marker_size=14))
 
   def test_part2_withExampleInput4_returnsMessage(self):
-    self.assertEqual(29, find_marker_position(self.examples[3], marker_size=14))
+    self.assertEqual(29, find_marker_position(buffer=self.examples[3][0], marker_size=14))
 
   def test_part2_withExampleInput5_returnsMessage(self):
-    self.assertEqual(26, find_marker_position(self.examples[4], marker_size=14))
+    self.assertEqual(26, find_marker_position(buffer=self.examples[4][0], marker_size=14))
 
   def test_part2_withPuzzleInput_returnsMessage(self):
-    self.assertEqual(3425, find_marker_position(self.input, marker_size=14))
+    self.assertEqual(3425, find_marker_position(buffer=self.input[0], marker_size=14))
 
 
 if __name__ == '__main__':
