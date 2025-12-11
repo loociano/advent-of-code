68a08eee65b470423f92ebe38f8b109a9e21d5b8
Luc Rubio <luc@loociano.com>
2025-12-11 22:44:18 +0100

    AoC 2025 day 4 part 1

---
+++ aoc2025/src/day04/python/solution.py
@@ -0,0 +1,52 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Sequence
+from common.python3.graph_utils import char_at, within_bounds
+from common.python3.types import Position
+
+_PAPER_ROLL = '@'
+
+
+def _is_roll(grid: Sequence[Sequence[str]], pos: Position) -> bool:
+  """Returns True if there is a roll of paper at the given position."""
+  return char_at(grid=grid, pos=pos) == _PAPER_ROLL
+
+
+def _can_access_roll(grid: Sequence[Sequence[str]], pos: Position) -> bool:
+  """Returns True if forklifts can access a roll at a given position.
+
+  Forklifts can only access a roll of paper if there are fewer than four rolls
+  of paper in the eight adjacent positions.
+  """
+  adj_rolls = 0
+  for dy in [-1, 0, 1]:
+    for dx in [-1, 0, 1]:
+      if dx == 0 and dy == 0:
+        continue  # Skip original position.
+      adj_pos = (pos[0] + dy, pos[1] + dx)
+      if not within_bounds(grid=grid, pos=adj_pos):
+        continue  # Skip out of bounds.
+      if char_at(grid=grid, pos=adj_pos) == _PAPER_ROLL:
+        adj_rolls += 1
+        if adj_rolls == 4:
+          return False
+  return True
+
+
+def count_rolls(grid: Sequence[Sequence[str]]) -> int:
+  return sum(1
+             for y in range(len(grid))
+             for x in range(len(grid[0]))
+             if _is_roll(grid=grid, pos=(y, x))
+             and _can_access_roll(grid=grid, pos=(y, x)))
---
+++ aoc2025/test/day04/python/test_solution.py
@@ -0,0 +1,32 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import unittest
+
+from aoc2025.src.day04.python.solution import count_rolls
+from common.python3.AdventOfCodeTestCase import AdventOfCodeTestCase
+
+
+class TestDaySolution(AdventOfCodeTestCase):
+  def __init__(self, *args, **kwargs):
+    super().__init__(__file__, *args, **kwargs)
+
+  def testPart1_withSampleInput(self):
+    self.assertEqual(13, count_rolls(grid=tuple(tuple(row) for row in self.examples[0])))
+
+  def testPart1_withPuzzleInput(self):
+    self.assertEqual(1344, count_rolls(grid=tuple(tuple(row) for row in self.input)))
+
+
+if __name__ == '__main__':
+  unittest.main()
